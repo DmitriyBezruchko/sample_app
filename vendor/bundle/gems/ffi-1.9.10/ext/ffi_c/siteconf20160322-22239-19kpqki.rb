ab066e40349a329daf1c533e249e29c787789176
Dima <bezruchko.dmitriy@gmail.com>
2016-03-22 00:17:24 +0200

    Initial commit

---
+++ vendor/bundle/gems/ffi-1.9.10/ext/ffi_c/siteconf20160322-22239-19kpqki.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20160322-22239-ur035p"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
